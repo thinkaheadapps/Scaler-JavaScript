6a0b4bb2458e1057046a2b99a827badc430f3149
Divya Rai <thinkaheadapps@gmail.com>
2024-06-05 11:43:14 +0530

    Higher Order Function - Map

--- Module6/HOF.js
+++ Module6/HOF.js
@@ -11,6 +11,8 @@ console.log(squareArray)
 
 //Higher order function - map - its already build in java script
 // Map will loop through every element of your array and will perform specific operations that you have provided
+//Map method will always return a new array with your result
+
 
 let num = [1 , 2 , 3, 4, 5];
 
@@ -19,3 +21,27 @@ let resultSquare = num.map(function(n) {
 })
 
 console.log(resultSquare);
+
+/*
+const transactions = [1000, 30000, 40000, 2000, -898, 3800, -4500];
+const inrToDollar = 80;
+*/
+
+const transactions = [1000, 30000, 40000, 2000, -898, 3800, -4500];
+const inrToDollar = 80;
+
+let inrToDollarArr = transactions.map(function(n) {
+    return inrToDollar * n;
+});
+
+console.log(inrToDollarArr);
+
+//Using arrow function
+
+let transactionsInDollar = transactions.map((num)=> {
+    return (inrToDollar * num).toFixed(0) ;
+})
+
+console.log(transactionsInDollar);
+
+
