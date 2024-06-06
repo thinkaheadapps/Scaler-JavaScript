4093d1b8c9bc6d7705c83d651de86c0c4ace0e56
Divya Rai <thinkaheadapps@gmail.com>
2024-06-06 19:25:16 +0530

    InheritanceClassical

--- Module7/InheritanceClassical.js
+++ Module7/InheritanceClassical.js
@@ -15,8 +15,11 @@ constructor(_name, _age, _classStrength) {
 
 }
 
-class Student {
-
+class Student extends Person {
+    constructor(_name, _age, _cgpa) {
+        super(_name , _age)
+        this.cgpa = _cgpa
+    }
 }
 
 let Person1 = new Person('Adam', 23);
@@ -24,4 +27,7 @@ console.log(Person1);
 
 let Teacher1 = new Teacher('Mark', 40, 30);
 console.log(Teacher1);
+
+let Student1 = new Student('Mark', 10, 8);
+console.log(Student1);
 
